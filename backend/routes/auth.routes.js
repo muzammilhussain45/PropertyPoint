5f98897ca003cce5d6e5a698585e63b5a8069df9
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-28 15:46:05 +0500

    Adding auth related routes

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+import {
+  register,
+  login,
+  getMe,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+} from '../controllers/auth.controller.js';
+import { protect } from '../middlewares/auth.middleware.js';
+
+const authRouter = express.Router();
+
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.get('/me', protect, getMe);
+authRouter.post('/verify-email', verifyEmail);
+authRouter.post('/forgot-password', forgotPassword);
+authRouter.post('/reset-password/:token', resetPassword);
+
+export default authRouter;
