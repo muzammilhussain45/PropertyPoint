0a2fd72999c00954a63589cda30885cb6b6e74da
Muzammil Hussain <muzammilch2021@gmail.com>
2026-08-30 17:37:19 +0500

    Adding inquiry routes

---
+++ backend/routes/inquiry.routes.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import {
+  sendInquiry,
+  getSellerInquiries,
+  markAsRead,
+} from '../controllers/inquiry.controller.js';
+import { protect, authorize } from '../middlewares/auth.middleware.js';
+
+const inquiryRouter = express.Router();
+
+inquiryRouter.post('/', protect, authorize('buyer'), sendInquiry);
+inquiryRouter.get('/seller', protect, authorize('seller'), getSellerInquiries);
+inquiryRouter.patch('/:id/read', protect, markAsRead);
+
+export default inquiryRouter;
