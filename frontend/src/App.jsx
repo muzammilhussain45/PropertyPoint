448891d18a6ac7ca36a639caaa7ac34d42e609d1
Muzammil Hussain <muzammilch2021@gmail.com>
2026-09-03 10:28:26 +0500

    Add forgot and reset password pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import PropertyDetails from "./pages/shared/PropertyDetails"
 import Register from "./pages/auth/Register"
 import VerifyEmail from "./pages/auth/VerifyEmail"
 import Login from "./pages/auth/Login"
+import ForgotPassword from "./pages/auth/ForgotPassword"
+import ResetPassword from "./pages/auth/ResetPassword"
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/verify-email" element={<VerifyEmail />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password/:token" element={<ResetPassword />} />
       <Route path="/" element={<LandingPage />} />
       <Route path="/properties" element={<Properties />} />
       <Route path="/property/:id" element={<PropertyDetails />} />
